a40ed7b79751584907ea72274ffc747e249efea0
kseniia <k.kseniiav@gmail.com>
2024-05-14 11:16:10 +0900

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const arr = users.filter(element => element.gender === gender).map(element =>
+    element.age).slice();
+    arr.reduce((element , item, index) => element + item, 0);
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return arr.reduce((element , item, index) => element + item, 0) / arr.length;
+  }
 }
